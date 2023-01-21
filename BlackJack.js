637f52a53345b281dd9a6b42b2b7e03601252a33
jcurti2 <joshua.curtis24@gmail.com>
2023-01-21 09:37:34 -0800

    Classes and their functions work

--- BlackJack.js
+++ BlackJack.js
@@ -30,11 +30,27 @@ cardValue() {
 }
 
 class Player{
-    constructor(player){
+    constructor(player,dealer){
         this.name = player;
         this.hand = [];
+        this.sum = 0
+        this.dealer = dealer
     }
-}
+    addCardToHand(card){
+        this.hand.push(card)
+       if(card.id == 1 ){
+        if(this.sum >= 11){
+            this.sum += 1;
+        }else {
+            this.sum+=11
+        }
+        }else if(card.id > 10){
+            this.sum += 10;
+    } else {
+        this.sum += card.id 
+    }
+    
+}}
 class Deck{
     cards = [];
     fillDeck(){
@@ -49,9 +65,12 @@ class Deck{
         
     }
     shuffleDeck(){
-
-    }
+        this.cards.sort(() => Math.random() - 0.5)
+        console.log(this.cards);
+    }   
     dealCard(){
+//pop 
+      return this.cards.pop();
 
     }
 
@@ -60,7 +79,19 @@ let player1 = new Player('Player1');
 
 let dealer = new Player('Dealer');
 
+// let joker = new Card(1, 6);
+// let joker2 = new Card(3, 11
+//     )
+let deck1 = new Deck;
 
-let deck1 = new Deck
 deck1.fillDeck();
-console.log(deck1.cards);
+
+deck1.shuffleDeck();
+deck1.shuffleDeck();
+
+// console.log(deck1.cards);
+// player1.handValue()
+player1.addCardToHand(deck1.dealCard());
+player1.addCardToHand(deck1.dealCard());
+console.log(player1.sum);
+console.log(player1.hand);
