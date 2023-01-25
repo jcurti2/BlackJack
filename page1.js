12963eea0f3649ec1accce56ca527b27ccc8ac3a
jcurti2 <joshua.curtis24@gmail.com>
2023-01-25 07:10:51 -0800

    updated btn css

--- page1.js
+++ page1.js
@@ -8,21 +8,15 @@
 
 function movingBtn()
 {
-    var randX = Math.floor(Math.random() * (window.innerWidth - 100));
-    var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    var randX = Math.floor(Math.random() * (window.innerWidth - 88));
+    var randY = Math.floor(Math.random() * (window.innerHeight - 88));
     caller.stop().animate({"left": randX + "px", "top": randY + "px"});
 
 }
 
-// $(document).ready(function() {
-    
-//     caller.on('mouseenter', foo);
-//     caller.on('click', function(){
-//         alert('clicked!');
-//     });
-    
-// });
 $(document).ready(function(){
 caller.on('mouseenter', movingBtn);
       });
+
+      //http://www.geeksforgeeks.org/different-ways-to-include-jquery-in-a-webpage/ source for moving button
       
